6d98f56acd54d8014e61eaddba6a53b547461751
unintendedbear <unintendedbear@gmail.com>
2017-09-27 12:54:42 +0200

    Ruby version of JALS is finished. It now shows the output as required. Closes #7

--- Ruby version/JALS.rb
+++ Ruby version/JALS.rb
@@ -22,7 +22,7 @@ require 'nokogiri'
 URL_STRING = "https://www.port-monitor.com/plans-and-pricing"
 
 class Scraper
-  attr_accessor :url, :document, :elements
+  attr_accessor :url, :document, :elements, :data
 
   def get_HTML_from_URL
     return Nokogiri::HTML(open(url))
@@ -33,20 +33,40 @@ class Scraper
   end
 
   def get_products_in_hash
+    data = Hash.new
+    id = 0
     elements.each do |product|
-      puts product.css("h2").text
-      puts product.css("dd")[0].text
-      puts product.css("dd")[1].text
-      puts product.css("dd")[2].text
-      puts product.css("dd")[3].text
-      puts product.css("a[class='btn btn-large btn-success']").text
+      data[id] = Hash.new
+      data[id]["monitors"] = product.css("h2").text
+      data[id]["check_rate"] = product.css("dd")[0].text.gsub!(/\D/, "")
+      data[id]["history"] = product.css("dd")[1].text.gsub!(/\D/, "")
+      data[id]["multiple_notifications"] = product.css("dd")[2].text
+      data[id]["push_notifications"] = product.css("dd")[3].text
+      data[id]["price"] = product.css("a[class='btn btn-large btn-success']").text.gsub!(/^\$/, "").gsub!(/\s\/\s\w+/, "")
+      id += 1
     end
+    return data
+  end
+
+  def visualise_hash
+    puts "[\n"
+    data.keys.each do |id|
+      puts "\t{"
+      data[id].keys.each do |key|
+        puts "\t\t#{key}:" + data[id][key]
+      end
+      puts "\t}"
+    end
+    puts "\n]"
   end
 end
 
 jals = Scraper.new
 jals.url = URL_STRING
+puts "JALS is now parsing https://www.port-monitor.com/plans-and-pricing"
 jals.document = jals.get_HTML_from_URL()
 jals.elements = jals.get_products()
-
-jals.get_products_in_hash()
+jals.data = jals.get_products_in_hash()
+puts "Your results:"
+jals.visualise_hash()
+puts "Thanks for using JALS!"
